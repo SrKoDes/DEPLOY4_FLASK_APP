5fcf96950ead110f36f3070847858ae51a3ed4f3
SrKoDes <desravines.kohiin@gmail.com>
2021-09-02 17:17:36 -0400

    unfinished koalas

--- doer.py
+++ doer.py
@@ -3,10 +3,4 @@ import sqlite3
 def add_Koala_Class():
     pass
 
-def welcome():
-    welcome_message = "Welcome to the Koala world!"
-    return welcome_message
 
-def choice():
-    choice_message = "Choose your class: Bear, Queenslander, Phascolarctos, Joey"
-    return(choice_message)
--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(['index.html', 'script.js', 'style.css'])
+    return render_template('index.html')
